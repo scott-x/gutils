ad13189e4743ed5c3f694f2e18c0ccf9cf154438
scottxiong <github.scott.bear@gmail.com>
2019-08-27 00:09:52 +0800

    fix bug

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -2,7 +2,7 @@
 * @Author: sottxiong
 * @Date:   2019-07-07 16:28:34
 * @Last Modified by:   scottxiong
-* @Last Modified time: 2019-08-26 23:57:53
+* @Last Modified time: 2019-08-27 00:08:58
  */
 package cmd
 
@@ -28,13 +28,14 @@ type Questions struct {
 }
 
 var (
-	questions     = &Questions{}
-	answers       = map[string]string{}
-	index     int = 0
-	did           = false
+	questions = &Questions{}
+	answers   = map[string]string{}
+	all       = 0
+	cmded     = 0
 )
 
 func AddQuestion(name, tip, retip, re string) *Questions {
+	all++
 	questions.qs = append(questions.qs, Question{name, tip, retip, re})
 	return questions
 }
@@ -64,21 +65,14 @@ func command(q *Question) {
 		}
 	}
 	//q.Do()
+	cmded++
 }
 
 func Exec() map[string]string {
 	//Exec()第二次执行的时候会出现bug
-	if did {
-		for _, question := range questions.qs {
-			index++
-			command(&question)
-		}
-		did = true
 
-	} else {
-		for _, question := range questions.qs[index:] {
-			command(&question)
-		}
+	for _, question := range questions.qs[cmded:all] {
+		command(&question)
 	}
 	return answers
 }

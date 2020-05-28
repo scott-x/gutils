41e3355c5f93081bb564c0ba560c5fe1f93e3fb9
scott-x <mbp98k@gmail.com>
2020-05-29 04:35:56 +0800

    update api func GetTables(sqlFileName string) *model.Tables

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/mattn/go-colorable v0.1.6 // indirect
 	github.com/otiai10/copy v1.0.1
 	github.com/schollz/progressbar/v3 v3.3.3
+	github.com/scott-x/sql v0.0.0-20190807034525-d46040909dc6 // indirect
 	github.com/spf13/viper v1.6.3
 	github.com/tealeg/xlsx v1.0.5
 	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7
--- parse/mysql.go
+++ parse/mysql.go
@@ -7,10 +7,10 @@
 package parse
 
 import (
-	_ "fmt"
 	"github.com/scott-x/gutils/fs"
 	"github.com/scott-x/gutils/model"
 	"strings"
+	"regexp"
 )
 
 var filed_keywords = []string{
@@ -22,62 +22,111 @@ var filed_keywords = []string{
 	"TEXT",
 }
 
-func GetTables(fileName string) *model.Tables {
+func GetTables(fileName string) *model.Tables{
 	tables := &model.Tables{}
-	var newArr []string
-	content := fs.ReadFile(fileName)
-	//按行存入数组中
-	arr := strings.Split(content, "\n")
-	for _, x := range arr {
-		if strings.Contains(strings.ToUpper(x), "CREATE TABLE") && strings.Contains(x, "(") {
-			newArr = append(newArr, x)
-		}
-		//过滤注释 # --
-		if strings.HasPrefix(strings.TrimSpace(x), "--") || strings.HasPrefix(strings.TrimSpace(x), "#") || strings.Contains(strings.ToUpper(x), "INSERT") {
+	str_arr := get_table_declaration(fileName)
+	for _, v := range str_arr {
 
-		} else if strings.Contains(x, ";") {
-			if strings.Contains(strings.ToUpper(x), "DATABASE") || strings.Contains(strings.ToUpper(x), "UPDATE") || strings.Contains(strings.ToUpper(x), "DELETE") || strings.Contains(strings.ToUpper(x), "DROP") {
+	    table := &model.Table{}
 
-			}
+	    //fields
+	    arr := strings.Split(v,"\n")
+	    for _,v := range arr {
+	    	if v[len(v)-1]=='(' {
+	    		//table start
+	    		table.Name = getTableName(v)
+	             
+	    	}else if strings.Contains(v,";") {
+	    		//table end
 
-		} else {
+	    	}else {
+	    		x, y := getType(v)
 
-			for _, y := range filed_keywords {
-				// 如果包含mysql type关键字 或者 有 CREATE TABLE comment( 等字样则保留
-				if strings.Contains(strings.ToUpper(x), y) {
-					newArr = append(newArr, x)
-				}
-			}
-
-		}
-	}
-	table := &model.Table{}
-	for _, z := range newArr {
-
-		//table start
-		if strings.Contains(strings.ToUpper(z), "CREATE TABLE") {
-			//add table
-			if table.Name != "" {
-				*tables = append(*tables, *table)
-			}
-			//reset table to nil
-			table = &model.Table{}
-			tb := getTableName(z)
-			table.Name = tb
-			// fmt.Println(tb)
-		} else {
-			f := &model.Field{}
-
-			field, t := getType(z)
-			f.Name = field
-			f.Type = t
-			// fmt.Println("field:" + field + ", type:" + t)
-			(*table).Fields = append((*table).Fields, *f)
-		}
-		//table end
+	    		(*table).Fields = append((*table).Fields,model.Field{x,y})
+	    	}
+	    }
+	    *tables  = append(*tables,*table)
 	}
 	return tables
 }
+
+func get_table_declaration(fileName string) []string{
+	c, _ := fs.ReadFile1(fileName)
+	//remove comment start with # or --
+	re_comment1 := regexp.MustCompile(`(\s{0,}--.*)`)
+	re_comment2 := regexp.MustCompile(`(\s{0,}#.*)`)
+	// re_comment3 := regexp.MustCompile(`(\s{0,}comment.*),`)
+	// re_comment4 := regexp.MustCompile(`(\s{0,}comment.*)`)
+	c = re_comment1.ReplaceAllString(c, "")
+	c = re_comment2.ReplaceAllString(c, "")
+	// c = re_comment3.ReplaceAllString(c, ",")
+	// c = re_comment4.ReplaceAllString(c, "")
+
+	//replace create table => CREATE TABLE
+	re_replace := regexp.MustCompile(`(create table)`)
+	c = re_replace.ReplaceAllString(c, "CREATE TABLE")
+	// fmt.Println(c)
+	// get the table comment
+	re := regexp.MustCompile(`CREATE TABLE(.*\n)(.*,\n)+(.*\n).*;`)
+	return re.FindAllString(c, -1)
+}
+
+// func GetTables(fileName string) *model.Tables {
+// 	tables := &model.Tables{}
+// 	var newArr []string
+// 	content := fs.ReadFile(fileName)
+// 	//按行存入数组中
+// 	arr := strings.Split(content, "\n")
+// 	for _, x := range arr {
+// 		if strings.Contains(strings.ToUpper(x), "CREATE TABLE") && strings.Contains(x, "(") {
+// 			newArr = append(newArr, x)
+// 		}
+// 		//过滤注释 # --
+// 		if strings.HasPrefix(strings.TrimSpace(x), "--") || strings.HasPrefix(strings.TrimSpace(x), "#") || strings.Contains(strings.ToUpper(x), "INSERT") {
+
+// 		} else if strings.Contains(x, ";") {
+// 			if strings.Contains(strings.ToUpper(x), "DATABASE") || strings.Contains(strings.ToUpper(x), "UPDATE") || strings.Contains(strings.ToUpper(x), "DELETE") || strings.Contains(strings.ToUpper(x), "DROP") {
+
+// 			}
+
+// 		} else {
+
+// 			for _, y := range filed_keywords {
+// 				// 如果包含mysql type关键字 或者 有 CREATE TABLE comment( 等字样则保留
+// 				if strings.Contains(strings.ToUpper(x), y) {
+// 					newArr = append(newArr, x)
+// 				}
+// 			}
+
+// 		}
+// 	}
+// 	table := &model.Table{}
+// 	for _, z := range newArr {
+
+// 		//table start
+// 		if strings.Contains(strings.ToUpper(z), "CREATE TABLE") {
+// 			//add table
+// 			if table.Name != "" {
+// 				*tables = append(*tables, *table)
+// 			}
+// 			//reset table to nil
+// 			table = &model.Table{}
+// 			tb := getTableName(z)
+// 			table.Name = tb
+// 			// fmt.Println(tb)
+// 		} else {
+// 			f := &model.Field{}
+
+// 			field, t := getType(z)
+// 			f.Name = field
+// 			f.Type = t
+// 			// fmt.Println("field:" + field + ", type:" + t)
+// 			(*table).Fields = append((*table).Fields, *f)
+// 		}
+// 		//table end
+// 	}
+// 	return tables
+// }
 
 func getType(str string) (string, string) {
 	//id VARCHAR(64) NOT NULL PRIMARY KEY,

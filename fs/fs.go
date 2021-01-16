bb3a4166cf1662056d236c593780d786a513df1b
scottxiong <github.scott.bear@gmail.com>
2021-01-16 09:42:18 +0800

    update

--- fs/fs.go
+++ fs/fs.go
@@ -2,7 +2,7 @@
 * @Author: scottxiong
 * @Date:   2019-07-25 16:10:54
 * @Last Modified by:   scottxiong
-* @Last Modified time: 2021-01-15 17:37:36
+* @Last Modified time: 2021-01-16 09:41:41
 ref https://stackoverflow.com/questions/8824571/golang-determining-whether-file-points-to-file-or-directory
 */
 package fs
@@ -114,16 +114,17 @@ func ListAllWithFileHeaders(folders []string) (*INFOS, int64, error) {
 					*infos = append(*infos, *i)
 
 				} else {
-					sum += 0
-					i := &INFO{}
-					header, err := zip.FileInfoHeader(info)
-					if err != nil {
-						return err
-					}
-					i.Header = header
-					i.Path = path
-					i.Size = 0
-					*infos = append(*infos, *i)
+					//folder
+					// sum += 0
+					// i := &INFO{}
+					// header, err := zip.FileInfoHeader(info)
+					// if err != nil {
+					// 	return err
+					// }
+					// i.Header = header
+					// i.Path = path
+					// i.Size = 0
+					// *infos = append(*infos, *i)
 				}
 
 				return nil

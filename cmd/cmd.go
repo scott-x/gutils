a88a11e28b46ff6330ea5d36a66e0d87cf5cbced
scottxiong <github.scott.bear@gmail.com>
2022-01-12 20:50:12 +0800

    update API

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -2,19 +2,20 @@
 * @Author: sottxiong
 * @Date:   2019-07-07 16:28:34
 * @Last Modified by:   scottxiong
-* @Last Modified time: 2019-12-27 00:58:13
+* @Last Modified time: 2022-01-12 20:43:34
  */
 package cmd
 
 import (
 	"bufio"
-	"github.com/fatih/color"
-	"github.com/scott-x/gutils/cl"
-	"github.com/scott-x/gutils/model"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
+
+	"github.com/fatih/color"
+	"github.com/scott-x/gutils/cl"
+	"github.com/scott-x/gutils/model"
 )
 
 var (
@@ -23,6 +24,10 @@ var (
 	all       = 0
 	cmded     = 0
 )
+
+type Tasker interface {
+	HandleItems() []string
+}
 
 func AddQuestion(name, tip, retip, re string) *model.Questions {
 	all++
@@ -132,6 +137,74 @@ func AddTask(tip string, color_option int, tasks ...string) string {
 	} else {
 		cl.BoldRed.Printf("Please input number from 1 to %d \n", tasks_length)
 		return ""
+	}
+}
+
+//return selected num, if invalid return -1
+func AddTask2(tip string, color_option int, t Tasker) int {
+	display := t.HandleItems()
+	tasks_length := len(display)
+	//color selection
+	var my_color *color.Color
+	switch color_option {
+	case 1:
+		my_color = cl.BoldRed
+	case 2:
+		my_color = cl.BoldBlue
+	case 3:
+		my_color = cl.BoldMagenta
+	case 4:
+		my_color = cl.BoldYellow
+	case 5:
+		my_color = cl.BoldWhite
+	case 6:
+		my_color = cl.BoldCyan
+	case 7:
+		my_color = cl.BoldGreen
+	default:
+		my_color = cl.BoldCyan
+	}
+	//use default tips
+	if tip == "" {
+		if tasks_length == 1 {
+			my_color.Printf("What you expected to do?\n")
+		} else {
+			my_color.Printf("What you expected to do? Select the number 1-%d: \n", tasks_length)
+		}
+	} else {
+		//customed tip
+		my_color.Printf("%s\n", tip)
+	}
+
+	for k, t := range display {
+		cl.BoldCyan.Printf(" %d. %s\n", k+1, t)
+	}
+	cl.BoldRed.Printf("My selection is: ")
+	reader := bufio.NewReader(os.Stdin)
+	option, err := reader.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	option = strings.Trim(option, "\n")
+	re := `^[1-9][0-9]*$`
+	res := regexp.MustCompile(re)
+	str := res.FindAllString(option, -1)
+
+	if len(str) == 0 {
+		if tasks_length == 1 {
+			cl.BoldRed.Printf("Please input number 1 \n")
+			return -1
+		} else {
+			cl.BoldRed.Printf("Please input number from 1 to %d \n", tasks_length)
+			return -1
+		}
+	}
+	num, _ := strconv.Atoi(option)
+	if num > 0 && num <= tasks_length {
+		return num
+	} else {
+		cl.BoldRed.Printf("Please input number from 1 to %d \n", tasks_length)
+		return -1
 	}
 }
 
